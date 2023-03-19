0906bce455ed63bda55a0939afa54745cb111149
darius-seales75 <43653895+darius-seales75@users.noreply.github.com>
2023-03-18 20:50:21 -0400

    Delete elgg directory

--- elgg/mod/Marketplace/views/default/object/marketplace/elements/sidebar.php
+++
@@ -1,4 +0,0 @@
-<?php
-/**
- * Marketplace view sidebar
- */
--- elgg/mod/developers/views/default/developers/ajax_demo.html.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-if (!isset($vars['entity']) || !$vars['entity'] instanceof ElggSite) {
-	elgg_register_error_message('$vars not set by ajax.form()');
-}
-
-?><p>view demo</p>
